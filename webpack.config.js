9971c91b3e160ba46e3d61330d708bc05ae91382
dom void <doe@onet.pl>
2017-06-16 20:29:17 +0200

    clean environment set up

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './app.jsx',
+    output: {
+        filename: 'app.js'
+    },
+    watch: true,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
