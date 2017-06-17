e790bad39d2475cc05a9346a5024cb19310dd5a2
dom void <doe@onet.pl>
2017-06-17 23:01:04 +0200

    ajax is working

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 var app = $('#app');
-var url = 'http://pokeapi.co/api/v2/pokemon';
+var url = 'http://pokeapi.co/api/v2/pokemon/';
 
-
+var allPokemonsNo = 0;
+var allPokemons = [];
 
 function load() {
+    $.when(getNoOfPokemons()).done(function () {
+        $.ajax({
+            url: url,
+            method: 'GET',
+            data: {
+                // offset: offset,
+                limit: allPokemonsNo
+            }
+        }).done(function (response) {
+            allPokemons = response.results;
+            getPokemon(allPokemons[810].url);
+            console.log(response);
+        }).fail(function (error) {
+            console.log(error);
+        })
+    })
+}
+
+function getNoOfPokemons() {
+    return $.ajax({
+        url: url,
+        method: 'GET'
+    }).done(function (response) {
+        allPokemonsNo = response.count;
+        console.log(response.count);
+    }).fail(function (error) {
+        console.log(error);
+    })
+}
+
+function getPokemon(url) {
     $.ajax({
         url: url,
-        method: 'GET',
-        data: {
-            offset: 5
-        }
-    }).done(function(response){
+        method: 'GET'
+    }).done(function (response) {
         console.log(response);
-    }).fail(function(error){
+    }).fail(function(error) {
         console.log(error);
     })
 }
